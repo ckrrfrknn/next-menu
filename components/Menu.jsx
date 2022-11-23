cea22430745b23104a5727027cde4c5f5d84175f
ckrrfrknn <cakir.furkan@ogr.ahievran.edu.tr>
2022-11-23 16:06:01 +0300

    gerekli hatalar düzeltildi

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 import "../pages/_app.js";
 const Menu = () => {
   return (
-    <nav>
-      <Link href="/">Anasayfa</Link>
-      <Link href="/about">Hakkımızda</Link>
-      <Link href="/contact">İletişim</Link>
-    </nav>
+    <div>
+      <nav>
+        <Link href="/">Anasayfa</Link>
+        <Link href="/about">Hakkımızda</Link>
+        <Link href="/contact">İletişim</Link>
+      </nav>
+    </div>
   );
 };
 export default Menu;
